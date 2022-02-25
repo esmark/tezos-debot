2872197ede0847edc03bfe670141fcae2673645e
Камиль <esmark@mail.ru>
2022-02-25 21:35:04 +0300

    rel 0.0.3: upgrade all functions, add few tech functions temporary

--- debots/Tezos.sol
+++ debots/Tezos.sol
@@ -1,7 +1,8 @@
-pragma ton-solidity >=0.40.0;
+pragma ton-solidity >=0.57.3;
 pragma AbiHeader expire;
 pragma AbiHeader time;
 pragma AbiHeader pubkey;
+
 import "./interfaces/Debot.sol";
 import "./interfaces/AddressInput/AddressInput.sol";
 import "./interfaces/Menu/Menu.sol";
@@ -9,12 +10,25 @@ import "./interfaces/Network/Network.sol";
 import "./interfaces/Terminal/Terminal.sol";
 import "./interfaces/UserInfo/UserInfo.sol";
 
+
 contract TezosContract is Debot {
     bytes m_icon;
-    address m_wallet; //
+    address m_wallet; //my Everscale address
+    string tezos_address; //Tezos address
+    string tezos_public_key; //Tezos public key
+    string tezos_private_key; //Tezos private key
 
-    mapping(address => string) tezos_wallet;
-    //uint8 constant DECIMAL = 6;
+    mapping(uint256 => string) tezos_wallet;
+
+    string constant DOMAIN = "https://rpc.hangzhounet.teztnets.xyz/"; //set test or main RPC domain
+
+	// Modifier that allows to accept some external messages
+	modifier checkPubkey {
+		// Check that contract's public key is set
+		require(msg.pubkey() != 0, 101);
+		tvm.accept();
+		_;
+	}
 
     function setIcon(bytes icon) public {
         require(msg.pubkey() == tvm.pubkey(), 100);
@@ -35,35 +49,90 @@ contract TezosContract is Debot {
     }
 
     function menuAddress() public {
-        AddressInput.get(tvm.functionId(setTezosAddress), "Enter Tezos wallet address:");
-        // TODO: continue here
+        //AddressInput.get(tvm.functionId(setTezosAddress), "Enter Tezos address:");
+        if (tezos_address != "") {
+            Terminal.print(0, format("Tezos address is {}. Reset it?", tezos_address));
+        }
+        
+        Terminal.input(tvm.functionId(setTezosAddress), "Enter Tezos address", false);
     }
 
     function menuBalance() public {
-        // TODO: continue here
-        string[] headers;
-        string url = "https://rpc.hangzhounet.teztnets.xyz/chains/main/blocks/head/context/contracts/";
-        url += "tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9";
-        url += "/balance";
-        //headers.push("Content-Type: application/x-www-form-urlencoded");
-        Network.get(tvm.functionId(setResponse), url, headers);
+
+        if (tezos_address == "") {
+            Terminal.input(tvm.functionId(setTezosAddress), "Firstly enter Tezos address", false);
+        } 
+        else {
+            string[] headers;
+            string url = DOMAIN + "chains/main/blocks/head/context/contracts/";
+            url += tezos_address; //url += "tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9";
+            url += "/balance";
+
+            Network.get(tvm.functionId(setResponse), url, headers);
+        }
     }
 
     function menuSend() public {
-        // TODO: continue here
-        UserInfo.getAccount(tvm.functionId(setDefaultAccount));
+
+        if (tezos_address == "") {
+            Terminal.input(tvm.functionId(setTezosAddress), "Firstly enter Tezos address", false);
+        }
+        else {
+            string[] headers;
+            string url = DOMAIN + "chains/main/blocks/head/context/contracts/";
+            url += tezos_address;
+            //url += "/balance";
+            
+            // TODO: continue here
+            //headers.push("Content-Type: application/x-www-form-urlencoded");
+            //Network.get(tvm.functionId(setResponse), url, headers);
+        } 
     }
 
-    function setTezosAddress(address value, string tezos_address) public {
-        // TODO: continue here 
-        tezos_address = "tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9";
-        tezos_wallet[value] = tezos_address;
-        Terminal.print(0, format("Tezos account {}", tezos_address));
+    function setTezosWallet(string value) public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+        tezos_address = value;
     }
 
-    function setDefaultAccount(address value) public {
-        Terminal.print(0, format("User account {}", value));
-        m_wallet = value;
+    function setTezosMapping(string value) public checkPubkey {
+        //require(msg.pubkey() == tvm.pubkey(), 100);
+        uint256 tvm_pubkey = tvm.pubkey();
+        tvm.accept();
+        tezos_wallet[tvm_pubkey] = value;
+    }
+
+    function setTezosAddress(string value) public {
+        //require(msg.pubkey() == tvm.pubkey(), 100);
+        require(value != "", 102);
+        //tvm.accept();
+
+        tezos_address = value;
+        //tezos_wallet[value] = tezos_address;
+
+        // TODO: verify correct address format
+        Terminal.print(tvm.functionId(getTezosAddress), format("Tezos address: {}", tezos_address));
+        
+        mainMenu();
+    }
+
+    function getTezosAddress() public view returns (string wallet) {
+        wallet = tezos_address;
+    }
+
+    function getTezosMapping() public view returns (string wallet) {
+        uint256 tvm_pubkey = tvm.pubkey();
+        wallet = tezos_wallet[tvm_pubkey];
+    }
+
+    function getTechInfo() public view returns (uint256 msg_pubkey, uint256 tvm_pubkey) {
+
+        msg_pubkey = msg.pubkey();
+        //Terminal.print(0, format("msg.pubkey: {}", msg_pubkey));
+        tvm_pubkey = tvm.pubkey();
+        //Terminal.print(0, format("tvm.pubkey: {}", tvm_pubkey));
+
     }
 
     function setResponse(int32 statusCode, string[] retHeaders, string content) public {
@@ -73,8 +142,9 @@ contract TezosContract is Debot {
             Terminal.print(0, hdr);
         } */
         // TODO: deserialize content from json to structure using Json interface.
-        //uint256 mod_contant = content * DECIMAL;
-        Terminal.print(0, content);
+        Terminal.print(0, format("Balance, nXTZ: {}", content));
+
+        mainMenu();
     }
 
     function getRequiredInterfaces() public view override returns (uint256[] interfaces) {
@@ -84,10 +154,10 @@ contract TezosContract is Debot {
     function getDebotInfo() public functionID(0xDEB) view override returns(
         string name, string version, string publisher, string key, string author,
         address support, string hello, string language, string dabi, bytes icon) {
-        name = "Tezos RPC Json DeBot";
-        version = "0.0.1";
+        name = "Tezos Wallet DeBot";
+        version = "0.0.3";
         publisher = "Equity philosophers";
-        key = "Tezos RPC Json DeBot";
+        key = "Tezos Wallet DeBot";
         author = "Equity philosophers";
         support = address(0);
         hello = "Please select a Menu Item.";
